aa203b1a257cb5b7e05015739ac7bed32dd7a783
Knox Wong <wongknox@gmail.com>
2025-07-27 02:30:08 -0500

    Add useMutation

---
+++ src/api/UseMutation.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useApi } from "./ApiContext";
+
+export default function useMutation(method, resource, tagsToInvalidate) {
+  const { request, invalidateTags } = useApi();
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const mutate = async (body) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const result = await request(resource, {
+        method,
+        body: JSON.stringify(body),
+      });
+      setData(result);
+      invalidateTags(tagsToInvalidate);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { mutate, data, loading, error };
+}
