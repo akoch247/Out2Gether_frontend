0120bed0f61bf1f9311a864da037ccbefc1e7176
Knox Wong <wongknox@gmail.com>
2025-07-28 01:57:05 -0500

    Adjust cart button styling and account route

--- src/layout/Topbar.jsx
+++ src/layout/Topbar.jsx
@@ -4,20 +4,32 @@ import { CgProfile } from "react-icons/cg";
 import { Link } from "react-router-dom";
 
 export default function Topbar() {
-    return (
-        //topbar container for top of page
-        <div className="d-flex justify-content-end align-items-center py-4 py-3 border-bottom bg-light">
-            <div className="d-flex gap-4 align-items-center">
-                {/*Account Styling*/}
-                <Link to="/favorites" className="d-flex align-items-center text-dark text-decoration-none fs-5">
-                    <CgProfile size={30} className="me-2"/> 
-                    Account
-                </Link>
-                {/* Cart Button Styling */}
-                <Link to="/cartpage" className="btn btn-danger d-flex align-items-center fw-semibold mx-5 fs-5"> 
-                    Cart
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    //topbar container for top of page
+    <div className="d-flex justify-content-end align-items-center py-4 py-3 border-bottom bg-light">
+      <div className="d-flex gap-4 align-items-center">
+        {/*Account Styling*/}
+        <Link
+          to="/account"
+          className="d-flex align-items-center text-dark text-decoration-none fs-5"
+        >
+          <CgProfile size={30} className="me-2" />
+          Account
+        </Link>
+        {/* Cart Button Styling */}
+        <Link
+          to="/cartpage"
+          className="btn btn-danger d-flex align-items-center fw-semibold mx-5 fs-5"
+          style={{
+            backgroundColor: "#F35242",
+            borderColor: "#F35242",
+            paddingLeft: "30px",
+            paddingRight: "30px",
+          }}
+        >
+          Cart
+        </Link>
+      </div>
+    </div>
+  );
 }
