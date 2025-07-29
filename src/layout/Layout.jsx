14a40d31d54b3b568f1dcc96ee72b9b1422efc6c
Knox Wong <wongknox@gmail.com>
2025-07-28 20:44:31 -0500

    Adjust layout for main content

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,11 +4,13 @@ import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
 export default function Layout() {
-    return (
-        <div>
-            <Sidebar />
-            <Topbar />
-            <main><Outlet /></main>
-        </div>
-    );
+  return (
+    <div>
+      <Sidebar />
+      <Topbar />
+      <main className="main-content" style={{}}>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
