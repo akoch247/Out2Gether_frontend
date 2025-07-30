8fc1139f4065a6760c69a273b78e79e60716eb9e
Adriana Koch <akoch@oberlin.edu>
2025-07-30 19:02:15 -0400

    add account page

--- src/accounts/AccountPage.jsx
+++ src/accounts/AccountPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CreateEventPage from "../events/CreateEventPage";
 import EditEventPage from "../events/EditEventPage";
 import MyPosts from "./MyPosts";
+import "./AccountsPage.css";
 
 export default function AccountPage() {
     console.log("account page loaded")
--- src/events/CreateEventPage.jsx
+++ src/events/CreateEventPage.jsx
@@ -1,10 +1,11 @@
 // A page with form to post a new event
 import React from "react";
 
+
 export default function CreateEventPage() {
     return (
         //Outer container for Make A Post 
-        <div className="make-post-container container mt-5 mb-5">
+        <div className="make-pot-container container mt-5 mb-5">
             <div className="post-form-wrapper p-4">
                 <h2 className="mb-4">Make a Post</h2>
             </div>
