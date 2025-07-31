a82b6035b3de3b93dd1c503a71a2e0ebe65443cf
Knox Wong <wongknox@gmail.com>
2025-07-30 19:08:25 -0500

    Make reusable button for filter bar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 
-export default function Button() {
-    return (
-        <>
-             <h1>Button</h1>
-        </>
-    );
+export default function Button({ children, ...props }) {
+  return (
+    <button type="button" className="btn btn-primary rounded-pill" {...props}>
+      {children}
+    </button>
+  );
 }
