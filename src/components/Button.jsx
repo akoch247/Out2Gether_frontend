5ed43b55cf0e8d584a55d64c48ae1c60bbcc59d1
Knox Wong <wongknox@gmail.com>
2025-07-30 19:13:53 -0500

    Create reusable button for filter bar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,17 @@ import React from "react";
 
 export default function Button({ children, ...props }) {
   return (
-    <button type="button" className="btn btn-primary rounded-pill" {...props}>
+    <button
+      type="button"
+      className="btn btn-primary rounded-pill"
+      style={{
+        backgroundColor: "#28BCB3",
+        color: "white",
+        borderColor: "#28BCB3",
+        fontWeight: "bold",
+      }}
+      {...props}
+    >
       {children}
     </button>
   );
