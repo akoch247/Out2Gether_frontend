c0d5d22997ad5caa3d0052c9c367402c3cd07f78
Adriana Koch <akoch@oberlin.edu>
2025-08-04 17:40:45 -0400

    fix buttons for my posts

--- src/accounts/MyPosts.jsx
+++ src/accounts/MyPosts.jsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function MyPosts() {
     return (
         <div className="make-pot-container container mt-5 mb-5 bg-white rounded p-4">
-            <div className="post-form-wrapper p-4">
-                <h1 className="mb-5 pb-5">My posts</h1>
-
-                <div className="d-inline-flex p-2">
-                    Post an Event
+            <div className="d-flex justify-content-between align-items-center mb-4">
+                <h1 className="mb-5">My posts</h1>
                 </div>
-                
+
+                <div className="d-flex justify-content-between align-items-center mb-4 flex-wrap">
+                    <div className="d-flex flex-wrap gap-2">
+                        <Button className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0">
+                            Date
+                        </Button>
+                        <Button className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0">
+                            Category
+                        </Button>
+                        <Button className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0">
+                            Price
+                        </Button>
+                    </div>
+                    <Button className="fw-bold px-4 py-2 mt-2 mt-md-0"
+                        style={{backgroundColor: "#fbbf24", color: "#000", border: "none", borderRadius: "10px"}}>Post an Event</Button>
                 </div>
             </div>
     )
