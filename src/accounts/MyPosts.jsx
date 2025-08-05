67a03c0cec974ecbd2325d5b0e849219a3d61362
Knox Wong <wongknox@gmail.com>
2025-08-04 20:53:09 -0500

    Added filter bar and yellow button components

--- src/accounts/MyPosts.jsx
+++ src/accounts/MyPosts.jsx
@@ -6,15 +6,16 @@ import { useApi } from "../api/ApiContext";
 import EventCard from "../components/EventCard";
 import { BsCalendar2Date } from "react-icons/bs";
 import { Button } from "react-bootstrap";
+import FilterBar from "../components/Filterbar";
+import YellowButton from "../components/YellowButton";
 
-export default function MyPosts( {title, fromPath}) {
-    const [posts, setPosts] = useState([]);
+export default function MyPosts({ title, fromPath }) {
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { request } = useApi();
   const [page, setPage] = useState(1);
   const limit = 12;
-
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -33,72 +34,41 @@ export default function MyPosts( {title, fromPath}) {
 
   const handleDelete = async (id) => {
     try {
-        await request(`/posts/${id}`, { method: "DELETE" });
-        setPosts((prevPosts) => prevPosts.filter(post => post.id !== id));
+      await request(`/posts/${id}`, { method: "DELETE" });
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
     } catch (err) {
-        setError(err.message);
+      setError(err.message);
     }
-  }
-
+  };
 
   if (loading) return <p>Loading events...</p>;
   if (error) return <div className="alert alert-danger">{error}</div>;
-    return (
-        <div className="make-pot-container container mt-1 bg-white rounded p-4">
-            <div className="d-flex justify-content-between align-items-center">
-                <h1 className="mb-4">My posts</h1>
-                </div>
+  return (
+    <div className="make-pot-container container mt-1 bg-white rounded p-4">
+      <div className="d-flex justify-content-between align-items-center">
+        <h1 className="mb-4">My posts</h1>
+      </div>
 
-                <div className="d-flex justify-content-between align-items-center mb-5 flex-wrap">
-                    <div className="d-flex flex-wrap gap-1">
-                        <Button    
-                            className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0" 
-                            style={{
-                                backgroundColor: "#28BCB3",
-                                color: "white",
-                                borderColor: "#28BCB3",
-                                fontWeight: "bold",
-                            }}>
-                                
-                                <BsCalendar2Date /> Date
-                        </Button>
-                        <Button 
-                            className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0" 
-                            style={{
-                                backgroundColor: "#28BCB3",
-                                color: "white",
-                                borderColor: "#28BCB3",
-                                fontWeight: "bold",
-                        }}>
-                        Category
-                        </Button>
-                        <Button 
-                        className="btn-info text-white fm-semibold px-3 py-2 rounded-pill border-0"
-                        style={{
-                            backgroundColor: "#28BCB3",
-                            color: "white",
-                            borderColor: "#28BCB3",
-                            fontWeight: "bold",
-                          }}>
-                        Price
-                        </Button>
-                    </div>
-                    <Link to="/createeventpage"
-                        className="fw-bold px-4 py-2 mt-2 text-decoration-none"
-                        style={{backgroundColor: "#fbbf24", color: "#000", border: "none", borderRadius: "10px"}}>Post an Event</Link>
-                </div>
-                <div className="bg-white rounded p-1 pt-0">
-                    <h1 className="mb-4">{title}</h1>
-                    <div className="row g-4">
-                        {posts.map((post) => (
-                            <div key={post.id} className="col-12">
-                                <EventCard post={post} fromPath={"myposts"} onDelete={handleDelete} />
-                            </div>
-                         ))}
-                    </div>
-
-                </div>
-
+      <div className="d-flex justify-content-between align-items-center mb-5 flex-wrap">
+        <FilterBar />
+        <Link to="/createeventpage">
+          <YellowButton>Post an Event</YellowButton>
+        </Link>
+      </div>
+      <div className="bg-white rounded p-1 pt-0">
+        <h1 className="mb-4">{title}</h1>
+        <div className="row g-4">
+          {posts.map((post) => (
+            <div key={post.id} className="col-12">
+              <EventCard
+                post={post}
+                fromPath={"myposts"}
+                onDelete={handleDelete}
+              />
             </div>
-    )
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
