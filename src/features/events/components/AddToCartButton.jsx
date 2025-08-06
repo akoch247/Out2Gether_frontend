17ddd345aa652c382fa14d6ab47497a23c59a9c7
Ethan Toups <ethantoups05@gmail.com>
2025-08-06 17:37:19 -0500

    Updated cart button

---
+++ src/features/events/components/AddToCartButton.jsx
@@ -0,0 +1,23 @@
+import useMutation from "../../../hooks/useMutation";
+import BlueButton from "../../../components/BlueButton";
+
+export default function AddToCartButton({ post }) {
+  const { mutate: addToCart, loading: isAddingToCart } = useMutation(
+    "POST",
+    "/cart",
+    ["cart", "cartButton"]
+  );
+
+  const handleAddToCart = () => {
+    if (isAddingToCart || !post) return;
+    addToCart({ post_id: post.id });
+  };
+
+  return (
+    <>
+      <BlueButton onClick={handleAddToCart} disabled={isAddingToCart}>
+        {isAddingToCart ? "Adding..." : "Add to Cart"}
+      </BlueButton>
+    </>
+  );
+}
